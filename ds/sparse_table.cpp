64208072e9eb7bc94430a2ac728d9718a52d1e20
Alejandro Naser Pastoriza <alejandro.naser@imdea.org>
2020-05-04 01:27:29 +0200

    update sparse table

--- ds/sparse_table.cpp
+++ ds/sparse_table.cpp
@@ -1,34 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define mfor(i,a,b) for(int i=(a);i<(b);i++)
+struct SparseTable{
+    int (*op)(int,int);
+    vector<vector<int>> st;
+    int query(int l, int r){
+        int k=31-__builtin_clz(r-l+1);
+        return op(st[k][l],st[k][r-(1<<k)+1]);
+    }
+    SparseTable(vector<int>A,int(*_op)(int,int)){
+        op=_op;int N=(int)A.size();int P=(int)log2(N)+1;
+        st.assign(P,vector<int>(N));
+        for(int i=0;i<N;i++)
+            st[0][i]=A[i];
+        for(int p=1;p<P;p++)
+            for(int i=0;i<N;i++)
+                st[p][i]=op(st[p-1][i],st[p-1][i+(1<<(p-1))]);
+    }
+};
 
-#define op min
-int P=4, N=1e1;
-int st[4][(int)1e1];
-// 2^P > N
-void st_init(int* A){
-    mfor(i,0,N)
-        st[0][i]=A[i];
-    mfor(p,1,P)
-        mfor(i,0,N)
-            st[p][i]=op(st[p-1][i],st[p-1][i+(1<<(p-1))]);
-}
-int st_query(int l, int r){
-    int k=31-__builtin_clz(r-l+1);
-    return op(st[k][l],st[k][r-(1<<k)+1]);
+int _min(int a, int b) {
+    return min(a, b);
 }
 
 void solve() {
-    int A[]={9,6,2,1,7,10,8,3,5,4};
-    st_init(A);
-    cout<<st_query(0,4)<<'\n';
-    cout<<st_query(5,9)<<'\n';
-    cout<<st_query(0,9)<<'\n';
+    vector<int> A = {9, 6, 2, 1, 7, 10, 8, 3, 5, 4};
+    SparseTable st(A, _min);
+
+    cout << st.query(0,4) << '\n';
+    cout << st.query(5,9) << '\n';
+    cout << st.query(0,9) << '\n';
 }
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
     solve();
     return 0;
 }
