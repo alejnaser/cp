4e178e01635745b89733cd768fce7de155d73e7f
Alejandro Naser Pastoriza <alejnaser@MacBook-Pro-de-Alejandro.local>
2021-03-08 20:13:21 +0100

    update dijkstra

--- algorithms/graphs/dijkstra.cpp
+++ algorithms/graphs/dijkstra.cpp
@@ -1,51 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define INF 1e9+5
-#define PB push_back
+#define INF 1e18
+typedef long long ll;
 
-int n;
-vector<int> dist(100005, INF);
-vector<pair<int, int>> adj[100005];
+// complexity: O(n + m log m)
 
-void dijkstra(int x) {
+void solve() {
+    int n, m; cin >> n >> m;
     bitset<100005> vis;
-    priority_queue<pair<int, int>> q;
-    dist[x] = 0;
-    q.push({0, x});
+    vector<ll> dist(n + 1, INF);
+    vector<pair<int, int>> adj[n + 1];
+    priority_queue<pair<ll, int>> q;
+    for (int i = 0; i < m; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        adj[a].push_back({b, c});
+    }
+    dist[1] = 0;
+    q.push({0, 1});
     while (!q.empty()) {
         int a = q.top().second; q.pop();
         if (vis[a])
             continue;
-        vis[a] = 1;
+        vis[a] = true;
         for (auto u : adj[a]) {
-            int b = u.first, w = u.second;
+            int b = u.first;
+            ll w = u.second;
             if (dist[a] + w < dist[b]) {
                 dist[b] = dist[a] + w;
                 q.push({-dist[b], b});
             }
         }
     }
-}
-
-void solve() {
-    n = 5;
-    adj[1].PB({2, 5}); adj[2].PB({1, 5});
-    adj[1].PB({4, 9}); adj[4].PB({1, 9});
-    adj[1].PB({5, 1}); adj[5].PB({1, 1});
-    adj[2].PB({3, 2}); adj[3].PB({2, 2});
-    adj[3].PB({4, 6}); adj[4].PB({3, 6});
-    adj[4].PB({5, 2}); adj[5].PB({4, 2});
-
-    dijkstra(1);
     for (int i = 1; i <= n; i++)
-        printf("dist from (1) to (%d) = %d\n", i, dist[i]);
+        cout << dist[i] << ' ';
+    cout << '\n';
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-
     solve();
     return 0;
 }
