673200627b24a78c1df3c99f8e022ffaf57498a9
Alejandro Naser Pastoriza <alejnaser@MacBook-Pro-de-Alejandro.local>
2021-03-08 19:43:48 +0100

    update bellman-ford

--- algorithms/graphs/bellman_ford.cpp
+++ algorithms/graphs/bellman_ford.cpp
@@ -1,42 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define INF 1e9+5
-#define PB push_back
+#define INF 1e18
+typedef long long ll;
 
-int n;
-vector<int> dist(100005, INF);
-vector<tuple<int, int, int>> edg;
-
-void bellman_ford(int x) {
-    dist[x] = 0;
-    for (int i = 1; i <= n - 1; i++) {
-        for (auto e : edg) {
+void solve() {
+    int n, m; cin >> n >> m;
+    vector<tuple<int, int, int>> edges;
+    for (int i = 0; i < m; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        edges.push_back({a, b, c});
+    }
+    vector<ll> dist(n + 1);
+    for (int i = 1; i <= n; i++)
+        dist[i] = INF;
+    dist[1] = 0;
+    for (int i = 1; i < n; i++)
+        for (auto e : edges) {
             int a, b, w;
             tie(a, b, w) = e;
             dist[b] = min(dist[b], dist[a] + w);
         }
-    }
-}
-
-void solve() {
-    n = 5;
-    edg.PB({1, 2,  2});
-    edg.PB({1, 3,  3});
-    edg.PB({1, 4,  7});
-    edg.PB({2, 4,  3});
-    edg.PB({2, 5,  5});
-    edg.PB({3, 4, -2});
-    edg.PB({4, 5,  2});
-    bellman_ford(1);
     for (int i = 1; i <= n; i++)
-        printf("dist from (1) to (%d) = %d\n", i, dist[i]);
+        cout << dist[i] << ' ';
+    cout << '\n';
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-
     solve();
     return 0;
 }
